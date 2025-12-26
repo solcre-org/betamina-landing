ba59b4295a6071853b5ecb9e70b7a7f0d720f233
Mica <=>
2025-12-26 17:16:26 -0300

    Mobile nav y animaciones

---
+++ assets/js/fixed/animations.js
@@ -0,0 +1,31 @@
+function scrollAnimationsInit() {
+  gsap.set(".js-animate-in", {
+    opacity: 0,
+    y: 40
+  });
+
+  ScrollTrigger.batch(".js-animate-in", {
+    start: "top 80%",
+
+    onEnter: batch => {
+      gsap.to(batch, {
+        y: 0,
+        opacity: 1,
+        duration: 0.8,
+        ease: "power2.out",
+        stagger: 0.15
+      });
+    },
+
+    onLeaveBack: batch => {
+      gsap.to(batch, {
+        y: 40,
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.1
+      });
+    }
+  });
+}
+
+window.addEventListener("load", scrollAnimationsInit);
--- assets/js/fixed/interfaz.js
+++ assets/js/fixed/interfaz.js
@@ -23,8 +23,75 @@ function headerFixedInit() {
     });
 }
 
+function navInit() {
+  const $body = document.querySelector('body');
+  const $navToggle = document.getElementById('nav-toggle');
+  const $nav = document.getElementById('main-nav');
+  const $overlay = document.querySelector(".js-nav-overlay");
+  const mediaq = window.matchMedia("(max-width: 1199px)");
+  const $navItems = document.querySelectorAll(".js-nav-item");
+
+
+  function applyDelays() {
+
+    if (!mediaq.matches) {
+      $navItems.forEach(el => el.style.transitionDelay = "");
+      return;
+    }
+
+    if ($body.classList.contains("nav-open") || $navToggle.checked) {
+      $navItems.forEach((el, i) => {
+        const delay = 0.10 + i * 0.05;
+        el.style.transitionDelay = `${delay}s`;
+      });
+    } else {
+      $navItems.forEach(el => el.style.transitionDelay = "0s");
+    }
+  }
+
+  function hideNav() {
+    if ($navToggle.checked) $navToggle.checked = false;
+
+    $nav.classList.add('is-closing');
+    $nav.classList.remove('is-visible');
+    $body.classList.remove('nav-open');
+
+    applyDelays();
+  }
+
+  $navToggle.onchange = () => {
+    if ($navToggle.checked) {
+      $nav.classList.add('is-visible');
+      $body.classList.add('nav-open');
+    } else {
+      hideNav();
+    }
+
+    applyDelays();
+  };
+
+  if ($overlay) {
+    $overlay.addEventListener("click", () => {
+      hideNav();
+      $navToggle.checked = false;
+    });
+  }
+
+  $nav.addEventListener('animationend', (event) => {
+    if (event.target === $nav && $nav.classList.contains('is-closing')) {
+      $nav.classList.remove('is-closing');
+      $nav.classList.remove('is-visible');
+    }
+  });
+
+  mediaq.addEventListener("change", applyDelays);
+
+  applyDelays();
+}
+
+
 window.addEventListener('load', () => {
     headerFixedInit();
     ScrollTrigger.refresh();
+    navInit();
 });
-
