7adc808399ecee599869584ca6afb51554760eb0
bruna-ceppa-solcre <bruna.ceppa@solcre.com>
2026-02-06 13:27:33 -0300

    feat: Introduce new carousel, tabs, FAQs, dialcode, and form validation features, and remove `popup.html`.

--- assets/js/sitio/component.popup.js
+++ assets/js/sitio/component.popup.js
@@ -1,49 +1,40 @@
-
 function popupInit() {
+  const $openPopupLinks = document.querySelectorAll(".js-showPopup");
 
-    const $openPopupLinks = document.querySelectorAll('.js-showPopup');
+  if ($openPopupLinks.length > 0) {
+    $openPopupLinks.forEach((link) => {
+      const popupId = link.getAttribute("href").substring(1);
+      const $popup = document.getElementById(popupId);
+      const $closePopup = $popup.querySelectorAll(".js-hidePopup");
+      const $popupOverlay = $popup.querySelector(".js-popupOverlay");
 
-    if ($openPopupLinks.length > 0) {
-        $openPopupLinks.forEach(link => {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        $popup.classList.add("is-visible");
+      });
 
-            const popupId = link.getAttribute('href').substring(1);
-            const $popup = document.getElementById(popupId);
-            const $closePopup = $popup.querySelectorAll('.js-hidePopup');
-            const $popupOverlay = $popup.querySelector('.js-popupOverlay');
+      $closePopup.forEach((closedBtn) => {
+        closedBtn.addEventListener("click", function () {
+          if ($popup.classList.contains("is-visible")) {
+            $popup.classList.add("is-closing");
+          }
 
-            link.addEventListener('click', function (e) {
-                e.preventDefault();
-                $popup.classList.add("is-visible");
-            });
-
-            $closePopup.forEach(closedBtn => {
-
-                closedBtn.addEventListener('click', function () {                    
-
-                    if($popup.classList.contains("is-visible")){
-                        $popup.classList.add("is-closing");
-                    }
-
-                    $popupOverlay.addEventListener(
-                        'animationend',
-                        (e) => {
-                          if (e.target === $popupOverlay && $popup.classList.contains('is-closing')) {
-                            $popup.classList.remove('is-visible', 'is-closing');
-                          }
-                        },
-                        { once: true }
-                      );
-                });
-
-            });
-
-            
+          $popupOverlay.addEventListener(
+            "animationend",
+            (e) => {
+              if (
+                e.target === $popupOverlay &&
+                $popup.classList.contains("is-closing")
+              ) {
+                $popup.classList.remove("is-visible", "is-closing");
+              }
+            },
+            { once: true },
+          );
         });
-
-
-
-    }
-
+      });
+    });
+  }
 }
 
 popupInit();
